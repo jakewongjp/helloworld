b8df522255c45591661c25f51b0c864a7d693527
Jake Wong <jakewongjp@gmail.com>
2016-07-24 11:33:40 -0400

    First Apps

---
+++ HelloWorldApp/HelloWorldApp/ViewController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using UIKit;
+
+namespace HelloWorldApp
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			//btnTest.TouchDown += Test;
+
+			//(object sender, EventArgs args) => { 
+			
+			//};
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+
+		partial void TouchDownTest(Foundation.NSObject sender)
+		{
+			/*var view = new UIAlertView();
+			view.Title = "dfdfdfd";
+			view.Message = "gdfgdfgdf";
+			view.Show();*/
+		}
+
+
+	/*	private void Test(object sender, EventArgs args)
+		{
+			
+		}*/
+
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
